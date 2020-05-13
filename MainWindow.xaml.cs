bd63ef7915086b5a380977112125a600f6734627
zylch11 <waqas.ahmed11@protonmail.com>
2020-05-13 19:57:29 +0500

    OpenFileDialog with multiple files selection capability added

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,45 @@ namespace Alg_03
     /// </summary>
     public partial class MainWindow : Window
     {
+        //variables
+        public int fileCount = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            //string path = @"C:\Users\Waqas\source\repos\Alg_03\.gitignore";
+            //using (StreamReader sr = File.OpenText(path))
+            //{
+            //    string s;
+            //    while ((s = sr.ReadLine()) != null)
+            //    {
+            //        Console.WriteLine(s);
+            //    }
+            //    string text = System.IO.File.ReadAllText(path);
+            //    MessageBox.Show(count.ToString());
+            //}
+        }
+
+        private void btnOpenFiles_Click(object sender, RoutedEventArgs e)
+        {
+            fileCount++;
+            string[] fileArray = new string[10];
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (string filename in openFileDialog.FileNames)
+                {
+                    lbFiles.Items.Add(System.IO.Path.GetFileName(filename));
+                    fileArray[fileCount] = System.IO.Path.GetFullPath(filename);
+                }
+            }
+            MessageBox.Show(System.IO.File.ReadAllText(fileArray[fileCount]));
         }
     }
 }
